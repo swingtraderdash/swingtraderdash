c00a58bd05dc94d6722e2f928d635b08f37b0c5d
swingtraderdash <swingtraderdash@gmail.com>
2025-08-24 17:26:13 +0100

    Create firebaseConfig.js

---
+++ public/js/firebaseConfig.js
@@ -0,0 +1,20 @@
+// File: /public/js/firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBIUbM6ZSPGDh9AxU2ySN4Ldtzpka5RpCg",
+  authDomain: "swingtraderdash-1a958.firebaseapp.com",
+  projectId: "swingtraderdash-1a958",
+  storageBucket: "swingtraderdash-1a958.firebasestorage.app",
+  messagingSenderId: "27892139023",
+  appId: "1:27892139023:web:10f0679e33eb93b3744f7a"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence)
+  .then(() => console.log("[Firebase] 🧠 Session persistence set to local"))
+  .catch((err) => console.error("[Firebase] ❌ Failed to set persistence:", err));
