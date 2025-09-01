1ee37e0f775a83749db319c57da49f35c683d080
AndrewO123456789 <spreadbettingworld@gmail.com>
2025-09-01 14:24:24 +0100

    Push updated injex.js, firebaseConfig.js, and index.html for deploy

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// For cost control, you can set the maximum number of containers that can be
+// running at the same time. This helps mitigate the impact of unexpected
+// traffic spikes by instead downgrading performance. This limit is a
+// per-function limit. You can override the limit for each function using the
+// `maxInstances` option in the function's options, e.g.
+// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
+// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
+// functions should each use functions.runWith({ maxInstances: 10 }) instead.
+// In the v1 API, each function can only serve one request per container, so
+// this will be the maximum concurrent request count.
+setGlobalOptions({maxInstances: 10});
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+exports.pageGatekeeper = onRequest((req, res) => {
+  logger.info(`Incoming request path: ${req.path}`, {structuredData: true});
+  if (req.path === "/newpage.html") {
+    logger.info("✅ Valid page hit detected", {structuredData: true});
+    res.status(200).send("Access granted");
+  } else {
+    logger.warn("🚫 Unauthorized access attempt", {structuredData: true});
+    res.status(403).send("Access denied");
+  }
+});
+
