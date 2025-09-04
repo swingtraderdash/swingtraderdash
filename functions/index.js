a37bed72e78a04558f5b9bd8ed0b1236903d92b1
AndrewO123456789 <spreadbettingworld@gmail.com>
2025-09-04 12:22:59 +0100

    Fix duplicate admin declaration and add CORS setup

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,7 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const cors = require('cors')({ origin: true });
 const { setGlobalOptions } = require("firebase-functions");
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
