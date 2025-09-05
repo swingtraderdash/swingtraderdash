1ac99699c214294ed2fe9acfddabb0e53dd98aff
AndrewO123456789 <spreadbettingworld@gmail.com>
2025-09-05 19:55:55 +0100

    Update .eslintrc.js to disable additional ESLint rules

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -9,6 +9,13 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'linebreak-style': ['off'],
+    'linebreak-style': ['off'], // Already disabled for CRLF
+    'no-unused-vars': ['off'], // Disable unused variables check
+    'object-curly-spacing': ['off'], // Disable spacing inside curly braces
+    'quotes': ['off'], // Disable single/double quote enforcement
+    'max-len': ['off'], // Disable maximum line length
+    'comma-dangle': ['off'], // Disable trailing comma requirement
+    'arrow-parens': ['off'], // Disable parentheses requirement for arrow functions
+    'eol-last': ['off'], // Disable newline at end of file requirement
   },
 };
