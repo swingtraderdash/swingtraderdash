ce5c5bd3f4ab597437beefa62373e3354d741c53
swingtraderdash <swingtraderdash@gmail.com>
2025-09-26 19:40:26 +0100

    Delete functions/testGen2.js

--- functions/testGen2.js
+++
@@ -1,5 +0,0 @@
-import { onRequest } from 'firebase-functions/v2/https';
-
-export const testGen2 = onRequest({ region: 'us-central1' }, (req, res) => {
-  res.status(200).send('Gen 2 is alive.');
-});
