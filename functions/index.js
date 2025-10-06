d50da4a0d8e4d99e49938f5a395a24818e1bb6ee
swingtraderdash <swingtraderdash@gmail.com>
2025-10-06 14:39:05 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 import { onRequest, onCall } from 'firebase-functions/v2/https';
-import { onSchedule } from 'firebase-functions/v2/scheduler';
 import { logger } from 'firebase-functions';
 import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
@@ -113,6 +112,7 @@ async function loadDataForTicker(ticker, startDate, endDate) {
 
   return rowsInserted;
 }
+
 export const fetchTiingo = onCall(
   {
     region: 'us-central1',
